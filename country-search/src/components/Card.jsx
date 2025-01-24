a7abf7e24bfa2f7d454ff76ef101105d52f4ffb9
naveed69 <naveed.desai.69@gmail.com>
2025-01-24 09:55:50 +0530

    Fetching API

--- country-search/src/components/Card.jsx
+++ country-search/src/components/Card.jsx
@@ -1,7 +1,10 @@
-const Card = () => {
+const Card = ({ country, flag }) => {
+  console.log("inside card");
   return (
     <>
       <h1>Hello</h1>
+      <h1>{country}</h1>
+      <div>{flag}</div>
     </>
   );
 };
--- country-search/src/components/DisplayCountries.jsx
+++ country-search/src/components/DisplayCountries.jsx
@@ -5,6 +5,8 @@ const DisplayCountries = () => {
   const url =
     "https://countries-search-data-prod-812920491762.asia-south1.run.app/countries";
   const [countries, setCountires] = useState([]);
+  const [filterCountry, setFilterCountry] = useState([]);
+  const [search, setSearch] = useState("");
   useEffect(() => {
     const fetchApi = async () => {
       try {
@@ -17,10 +19,33 @@ const DisplayCountries = () => {
     };
     fetchApi();
   }, []);
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+  useEffect(() => {
+    const filtered = () => {};
+    filtered();
+  }, [search]);
+
   return (
     <>
-      <input type="text" className="search" placeholder="Search for Country" />
-      <Card countries />
+      <input
+        type="text"
+        onChange={(e) => handleChange(e)}
+        className="search"
+        placeholder="Search for Country"
+      />
+      <hr />
+      {filterCountry.length > 0 ? (
+        <Card country={filterCountry} />
+      ) : (
+        <>
+          {countries.map((countryData) => {
+            // console.log(countryData.common, countryData.png);
+            <Card country={countryData.common} flag={countryData.png} />;
+          })}
+        </>
+      )}
     </>
   );
 };
